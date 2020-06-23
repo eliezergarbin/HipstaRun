1c133c787af6768b69a2325b64652f5d68a32932
Eliezer Nascimento <elieserdariogarbin@gmail.com>
2020-06-23 17:54:39 -0300

    Aula02 Concluida

--- scripts/jogo/personagem.js
+++ scripts/jogo/personagem.js
@@ -1,50 +1,51 @@
-  class Personagem{
-
-    constructor(imagem, spriteDeSaidaX = 220, 
-            spriteDeSaidaY = 270, 
-            spriteFrameCount = 16, 
-            spriteFrameLine = 4,
-            spriteReduzirZoom = 2)
-    {
-
-        this.spriteDeSaidaX = spriteDeSaidaX; 
-        
-        this.spriteDeSaidaY = spriteDeSaidaY;
-
-        this.spriteReduzirZoom = spriteReduzirZoom; 
-
-        this.spriteFrameCount = spriteFrameCount;
-        
-        this.spriteFrameLine = spriteFrameLine; 
-
-        const genScript = new GerarScript(spriteDeSaidaX, spriteDeSaidaY, spriteFrameCount, spriteFrameLine);
-
-        this.matriz = genScript.SpriteArray;
-
-        this.frameAtual = 0;
-
-        this.imagem = imagem;
+class Personagem extends Animacao {
+    constructor(matriz, imagem, x, largura, altura, larguraSprite, alturaSprite, somPulo){
+      super(matriz, imagem, x, largura, altura, larguraSprite, alturaSprite);
+      this.somPulo = somDoPulo;
+  
+      this.yInicial = height - this.altura;
+      this.y = this.yInicial;
+      
+      this.velocidadeDoPulo = 0;
+      this.gravidade = 3;
+      this.qntPulos = 2;
     }
-
-    exibe(){
-        image(this.imagem, 
-            0, //x
-            height-this.spriteDeSaidaY/this.spriteReduzirZoom, //y
-            this.spriteDeSaidaX/this.spriteReduzirZoom, 
-            this.spriteDeSaidaY/this.spriteReduzirZoom, 
-            this.matriz[this.frameAtual][0], 
-            this.matriz[this.frameAtual][1], 
-            this.spriteDeSaidaX, 
-            this.spriteDeSaidaY);
-
-        this.anima();
-    }
-
-    anima(){
-        this.frameAtual++;
-
-        if(this.frameAtual >= this.spriteFrameCount -1){
-            this.frameAtual = 0;
+    
+    pula() {
+        if (this.qntPulos > 0) {
+        this.velocidadeDoPulo = - 31
+        this.qntPulos--;
+        this.somPulo.play();
+  
         }
+    } 
+
+    aplicaGravidade() {    
+      this.y += this.velocidadeDoPulo;
+      this.velocidadeDoPulo += this.gravidade;
+      
+      if(this.y > this.yInicial){
+        this.y = this.yInicial;
+        this.qntPulos = 2;    
+        
+      }
     }
-}
+    
+    estaColidindo(inimigo){
+      noFill()
+      const precisao = .65 ;
+      const colisao = collideRectRect(
+        this.x,
+        this.y,
+        this.largura * precisao,
+        this.altura * precisao,
+        inimigo.x,
+        inimigo.y,
+        inimigo.largura * precisao,
+        inimigo.altura * precisao);
+      
+      return colisao;
+      
+    }
+    
+  }
