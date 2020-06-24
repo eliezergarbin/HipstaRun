3ba0607093e45ee7f4c70918fb192952bf870122
Eliezer Nascimento <elieserdariogarbin@gmail.com>
2020-06-24 15:30:46 -0300

    adicionando inimigos voadores

--- sketch.js
+++ sketch.js
@@ -2,13 +2,17 @@
 let imagemCenario, 
   imagemPersonagem,
   imagemInimigo, 
+  imagemInimigoGrande;
+  imagemInimigoVoador;
   
   cenario, 
   somDoJogo,
   somDoPulo,
 
   personagem,
-  inimigo;
+  inimigo,
+  inimigoGrande,
+  inimigoVoador;
 
   const matrizInimigo = [
     [0, 0],
@@ -40,7 +44,6 @@ let imagemCenario,
     [208, 626],
     [312, 626],
   ]
-  
   const matrizPersonagem = [
     [0, 0],
     [220, 0],
@@ -59,12 +62,64 @@ let imagemCenario,
     [440, 810],
     [660, 810],
   ]
+  const matrizInimigoGrande = [
+    [0,0],
+    [400,0],
+    [800,0],
+    [1200,0],
+    [1600,0],
+    [0,400],
+    [400,400],
+    [800,400],
+    [1200, 400],
+    [1600, 400],
+    [0,800],
+    [400, 800],
+    [800, 800],
+    [1200, 800],
+    [1600, 800],
+    [0, 1200],
+    [400, 1200],
+    [800, 1200],
+    [1200, 1200],
+    [1600, 1200], 
+    [0, 1600],
+    [400, 1600],
+    [800, 1600],
+    [1200, 1600],
+    [1600, 1600],
+    [0, 2000],
+    [400, 2000],
+    [800, 2000],
+  ]
+  const matrizInimigoVoador = [
+    [0,0],
+    [200, 0],
+    [400, 0],
+    [0, 150],
+    [200, 150],
+    [400, 150],
+    [0, 300],
+    [200, 300],
+    [400, 300],
+    [0, 450],
+    [200, 450],
+    [400, 450],
+    [0, 600],
+    [200, 600],
+    [400, 600],
+    [0, 750],
+  ]
+
+  const inimigos = []
   
 
 function preload() {
   imagemCenario = loadImage('imagens/cenario/floresta.png');
   imagemPersonagem = loadImage('imagens/personagem/correndo.png');
   imagemInimigo = loadImage('imagens/inimigos/gotinha.png');
+  imagemInimigoGrande = loadImage('imagens/inimigos/troll.png')
+  imagemInimigoVoador = loadImage('imagens/inimigos/gotinha-voadora.png')
   somDoJogo = loadSound('sons/trilha_jogo.mp3');
   somDoPulo = loadSound('sons/somPulo.mp3');
 }
@@ -74,7 +129,15 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   cenario = new Cenario(imagemCenario,3);
   personagem = new Personagem(matrizPersonagem, imagemPersonagem, 0,30, 110, 135, 220, 270);
-  inimigo = new Inimigo(matrizInimigo, imagemInimigo, width-50, 30, 52, 52, 104, 104);
+
+  const inimigo = new Inimigo(matrizInimigo, imagemInimigo, width -50, 30, 52, 52, 104, 104, 10, 200);
+  const inimigoGrande = new Inimigo(matrizInimigoGrande, imagemInimigoGrande, width, 0, 200, 200, 400, 400, 10, 2000);
+  const inimigoVoador = new Inimigo(matrizInimigoVoador, imagemInimigoVoador, width, -52, 200, 52, 52, 200, 150, 10, 500);
+  
+  inimigos.push(inimigo)
+  inimigos.push(inimigoGrande)
+  inimigos.push(inimigoVoador)
+
   frameRate(40)
     somDoJogo.loop();
 }
@@ -93,6 +156,8 @@ function draw() {
   personagem.exibe();
   personagem.aplicaGravidade();
 
+  inimigoGrande.exibe();
+  inimigoGrande.move();
   inimigo.exibe();
   inimigo.move();
 
