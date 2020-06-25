d5088d75e41cb55e8622bf24f0f3ca967755b90f
Eliezer Nascimento <elieserdariogarbin@gmail.com>
2020-06-25 18:57:38 -0300

    aplicando CSS no botão "iniciar"

--- scripts/cenas/botaoGerenciador.js
+++ scripts/cenas/botaoGerenciador.js
@@ -4,10 +4,19 @@ class BotaoGerenciador {
         this.x = x;
         this.y = y;
         this.Botao = createButton(this.texto)
+        this.Botao.mousePressed( () => this._alteraCena())
+        this.Botao.addClass('botao-tela-inicial');
     }
 
     draw() {
         this.Botao.position(this.x, this.y)
+        this.Botao.center('horizontal')
+
+    }
+
+    _alteraCena(){
+        this.Botao.remove();
+        cenaAtual = 'jogo'
     }
 }
 
