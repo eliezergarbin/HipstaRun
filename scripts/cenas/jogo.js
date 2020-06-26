a74d3d3ec7a1d23e06a13655a294a094ad5bcf02
Eliezer Nascimento <elieserdariogarbin@gmail.com>
2020-06-26 16:47:35 -0300

    mapeando inimigos

--- scripts/cenas/jogo.js
+++ scripts/cenas/jogo.js
@@ -1,6 +1,24 @@
 class Jogo {
     constructor(){
-        this.inimigoAtual = 0;
+        this.indice = 0;
+        this.mapa = [
+            {
+                inimigo: 0,
+                velocidade: 10
+            },
+            {
+                inimigo: 1,
+                velocidade: 30
+            },
+            {
+                inimigo: 1,
+                velocidade: 15
+            },
+            {
+                inimigo: 2,
+                velocidade: 40
+            },
+        ]
     }
 
     setup() {
@@ -9,9 +27,9 @@ class Jogo {
         personagem = new Personagem(matrizPersonagem, imagemPersonagem, 0, 30, 110, 135, 220, 270);
         vida = new Vida(3, 3);
     
-        const inimigo = new Inimigo(matrizInimigo, imagemInimigo, width - 52, 30, 52, 52, 104, 104, 10, 100);
-        const inimigoVoador = new Inimigo(matrizInimigoVoador, imagemInimigoVoador, width - 52, 200, 100, 75, 200, 150, 10, 100);
-        const inimigoGrande = new Inimigo(matrizInimigoGrande, imagemInimigoGrande, width, 0, 200, 200, 400, 400, 15, 100);
+        const inimigo = new Inimigo(matrizInimigo, imagemInimigo, width - 52, 30, 52, 52, 104, 104, 10);
+        const inimigoVoador = new Inimigo(matrizInimigoVoador, imagemInimigoVoador, width - 52, 200, 100, 75, 200, 150, 10);
+        const inimigoGrande = new Inimigo(matrizInimigoGrande, imagemInimigoGrande, width, 0, 200, 200, 400, 400, 15);
        
         inimigos.push(inimigo)
         inimigos.push(inimigoGrande)
@@ -36,18 +54,20 @@ class Jogo {
         personagem.exibe();
         personagem.aplicaGravidade();
       
-        const inimigo = inimigos[this.inimigoAtual]
+        const linhaAtual = this.mapa[this.indice]
+        const inimigo = inimigos[linhaAtual.inimigo]
         const inimigoVisivel = inimigo.x < - inimigo.largura;
+
+        inimigo.velocidade = linhaAtual.velocidade;
      
-           inimigo.exibe()
-           inimigo.move()
+        inimigo.exibe()
+        inimigo.move()
      
         if(inimigoVisivel){
-           this.inimigoAtual++
-            if(this.inimigoAtual > 2){
-                this.inimigoAtual = 0 
+           this.indice++
+            if(this.indice > this.mapa.length - 1){
+                this.indice = 0 ;
             }
-          inimigo.velocidade = parseInt(random (10,30));
         }
 
         if(personagem.estaColidindo(inimigo)){
